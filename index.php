b7fca6a12091de280926f4728428f1df634de71c
davault <davetheriault@gmail.com>
2015-09-26 15:51:21 -0600

    HomePage 2

--- index.php
+++ index.php
@@ -2,12 +2,28 @@
 <html>
     <head>
         <title>CS313 Theriault</title>
+        <link rel="style.css" type="text/css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <div id="container">
             <h1>CS313 Assignments</h1>
+            <ol>
+                <li>Week 3</li>
+                <li>Week 4</li>
+                <li>Week 5</li>
+                <li>Week 6</li>
+                <li>Week 7</li>
+                <li>Week 8</li>
+                <li>Week 9</li>
+                <li>Week 10</li>
+                <li>Week 11</li>
+                <li>Week 12</li>
+                <li>Week 13</li>
+                <li>Week 14</li>
+            </ol>
+            
         </div>
     </body>
 </html>
