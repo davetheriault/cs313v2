ff76af965f563b9ee45722ffbbdfe9a72a5c4c28
davault <davetheriault@gmail.com>
2015-09-26 13:14:21 -0600

    HomePage

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CS313 Theriault</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div id="container">
+            <h1>CS313 Assignments</h1>
+        </div>
+    </body>
+</html>
