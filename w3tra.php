ebcc9f8b00fc2615fb2ff461ca2e5fe0dd5b038c
davault <davetheriault@gmail.com>
2015-10-03 22:22:56 -0600

    week 3 TRA

--- w3tra.php
+++ w3tra.php
@@ -21,20 +21,24 @@
                     
                 <form id="form1" action="welcome.php" method="post">
                     Name: <input type="text" name="name"><br>
+                    <br>
                     Email: <input type="email" name="email"><br>
+                    <br>
                     Major: <br>
-                    <input type="radio" name="major" value="Computer Science"><br>
-                    <input type="radio" name="major" value="Web Development and Design"><br>
-                    <input type="radio" name="major" value="Computer information Technology"><br>
-                    <input type="radio" name="major" value="Computer Engineering"><br>
+                    <input type="radio" name="major" value="Computer Science">Computer Science<br>
+                    <input type="radio" name="major" value="Web Development and Design">Web Development and Design<br>
+                    <input type="radio" name="major" value="Computer information Technology">Computer information Technology<br>
+                    <input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+                    <br>
                     Places you have visited: <br>
-                    <input type="checkbox" name="places" value="North America"><br>
-                    <input type="checkbox" name="places" value="South America"><br>
-                    <input type="checkbox" name="places" value="Europe"><br>
-                    <input type="checkbox" name="places" value="Asia"><br>
-                    <input type="checkbox" name="places" value="Australia"><br>
-                    <input type="checkbox" name="places" value="Africa"><br>
-                    <input type="checkbox" name="places" value="Antartica"><br>
+                    <input type="checkbox" name="places" value="North America">North America<br>
+                    <input type="checkbox" name="places" value="South America">South America<br>
+                    <input type="checkbox" name="places" value="Europe">Europe<br>
+                    <input type="checkbox" name="places" value="Asia">Asia<br>
+                    <input type="checkbox" name="places" value="Australia">Australia<br>
+                    <input type="checkbox" name="places" value="Africa">Africa<br>
+                    <input type="checkbox" name="places" value="Antarctica">Antarctica<br>
+                    <br>
                     Comments: <br>
                     <textarea name="comments" form="form1"></textarea>
                     
