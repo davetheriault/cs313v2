0712070f59aa5b227a7f686dc5e633deef25e765
davault <davetheriault@gmail.com>
2015-10-03 22:25:29 -0600

    week 3 TRA

--- w3tra.php
+++ w3tra.php
@@ -16,7 +16,6 @@
                 <div id="headerBox"><div id="headTitle">David Theriault's CS313 Site</div></div>
             </div>
             <div id="mainContain">
-            <div id="mainBox">
                 <h1>Week 3 Team Readiness Activity</h1>
                     
                 <form id="form1" action="welcome.php" method="post">
@@ -41,38 +40,9 @@
                     <br>
                     Comments: <br>
                     <textarea name="comments" form="form1"></textarea>
-                    
-Places you have visited (checkbox) with the following options:
-North America
-South America
-Europe
-Asia
-Australia
-Africa
-Antartica
-Comments (text area)
-                    
+                                     
                 </form>
-                <ul class="list0">
-                    <li><a href="">Week 3</a>
-                        <ul>
-                            <li><a href="w3tra.php">Team Readiness Activity</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="">Week 4</a></li>
-                    <li><a href="">Week 5</a></li>
-                    <li><a href="">Week 6</a></li>
-                    <li><a href="">Week 7</a></li>
-                    <li><a href="">Week 8</a></li>
-                    <li><a href="">Week 9</a></li>
-                    <li><a href="">Week 10</a></li>
-                    <li><a href="">Week 11</a></li>
-                    <li><a href="">Week 12</a></li>
-                    <li><a href="">Week 13</a></li>
-                    <li><a href="">Week 14</a></li>
-                </ul>
-            </div>  
-            <div id="mainBox2"></div>
+                
             </div>
         </div>
     </body>
