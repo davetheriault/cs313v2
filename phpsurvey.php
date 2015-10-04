30cc28e5ea13e2448b4be0685202e1a0e972789b
davault <davetheriault@gmail.com>
2015-10-04 12:00:45 -0600

    week 3 TRA

--- phpsurvey.php
+++ phpsurvey.php
@@ -21,7 +21,7 @@
                 <section>
                     <form action="phpsurveysubmit.php" method="post" id="tvform">
                 
-                    <h2>Out of the following, which brand makes the highest quality televisions?</h2><br><br>
+                    <h2>Out of the following, which brand makes the highest quality televisions?</h2><br>
                 
                     <input type="radio" name="brand" value="y">Sony<br>
                     <input type="radio" name="brand" value="g">Samsung<br>
@@ -29,7 +29,7 @@
                     <input type="radio" name="brand" value="c">Panasonic<br>
                     <br>                   
                 
-                    <h2>In your opinion, what type of television produces the best picture?</h2><br><br>
+                    <h2>In your opinion, what type of television produces the best picture?</h2><br>
                     
                     <input type="radio" name="type" value="d">LCD<br>
                     <input type="radio" name="type" value="p">Plasma<br>
@@ -39,7 +39,7 @@
                 
                     
                     <h2>Which of the following features would you most prefer in a television?</h2>
-                    <br><br>
+                    <br>
                     <input type="radio" name="feat" value="3">3D TV<br>
                     <input type="radio" name="feat" value="w">WiFi<br>
                     <input type="radio" name="feat" value="a">Smell-a-Vision<br>
@@ -47,13 +47,15 @@
                 
                     
                     <h2>How much would you spend on a new television?</h2>
-                    <br><br>
+                    <br>
                     <input type="radio" name="spend" value="l">Less than $400<br>
                     <input type="radio" name="spend" value="4">$400 to $700<br>
                     <input type="radio" name="spend" value="7">$700 to $1000<br>
                     <input type="radio" name="spend" value="m">More than $1000<br>
                
             </form>
+                    <br>
+                    <br>
                 <input type="submit" form="tvform"><br>
             
                 </section>
