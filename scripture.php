14010cbb297da4adb6ff7b6d9cc773fe10fb63a4
davault <davetheriault@gmail.com>
2015-10-12 21:09:00 -0600

    week 5 team r a

--- scripture.php
+++ scripture.php
@@ -1,7 +1,8 @@
 <?php
 require 'includes/dbconnection.php';
-
 ?>
+
+
 <?php $title = 'CS313 Theriault'; ?>
             
             <?php include 'includes/header.php'; ?>
@@ -12,10 +13,25 @@ require 'includes/dbconnection.php';
             <?php 
             foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row){
                 echo '<div class="block"><strong>'. $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
-                        . $row['content'] . '"</div>';
+                        . $row['content'] . '"</div><br><br>';
             }
             ?>
-            
+            <div>
+                Search for scripture by book:
+                <form id="searchBook" action="scripture.php" method="post">
+                    <input type="text" name="book">
+                    <input type="submit" value="Submit">
+                </form>
+                <?php 
+                    if (isset($_POST['book'])) {
+                        echo '<p>Scriptures from the book of ' . $_POST['book'] . ':<br>';
+                        foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures WHERE book = "' . $_POST['book'] . '"') as $results){
+                            echo '<div class="block"><strong>'. $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
+                        . $row['content'] . '"</div><br><br>';
+                        }
+                    }
+                ?>
+            </div>
         </div>
         </div>
         
