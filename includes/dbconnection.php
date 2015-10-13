2afa5930dd903215080152eda6b630412aaaf446
davault <davetheriault@gmail.com>
2015-10-12 20:05:51 -0600

    week 5 team r a

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -16,7 +16,7 @@ else
      $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
      $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
      $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-     $dbName = 'php';
+     $dbName = 'site';
      // …
 }
 
