8edf7324c12740fc55999aac43d221c0f2629545
davault <davetheriault@gmail.com>
2015-10-17 18:26:05 -0600

    Database Access

--- librarysearch.php
+++ librarysearch.php
@@ -1,3 +1,5 @@
+<?php include 'includes/library_db_connection.php'; ?>
+
 <?php $title = 'Library Search'; ?>
             
             <?php include 'includes/header.php'; ?>
@@ -6,15 +8,27 @@
                 <div id="mainBox">
                     <h1>Find a Book</h1>
                     
-                    <form id="searchBooks" action="librarysearch.php">
+                    <form id="searchBooks" action="librarysearch.php" method="get">
                         <input name="bkstring" type="text">
                         <select name="searchby">
                             <option value="title">by Title</option>
                             <option value="author">by Author</option>
                         </select>
                         <input type="submit" value="Submit">
-                        
+                        <a href="librarysearch.php?browse=true">Browse by Genre</a>
                     </form>
+                    
+                    <div id="results">
+                    <?php 
+                        if ($_GET['browse'] = 'true') {
+                            foreach ($db->query('SELECT DISTINCT genre FROM books') as $genre){
+                                echo '<h2><a href="librarysearch.php?genre='.$genre['genre'].'">'
+                                        . $genre['genre'] . '</a></h2>';
+                            }
+                        }
+                    
+                    ?>
+                    </div>
                 </div>  
             </div>
         </div>
