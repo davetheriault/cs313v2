2d5466ba909a6dad47597e6a055167b53f894cc4
davault <davetheriault@gmail.com>
2015-10-21 00:21:22 -0600

    Merge origin/master
    Conflicts:
    	scriptures2.php

--- scriptureadd.php
+++ scriptureadd.php
@@ -17,6 +17,7 @@
                          
                         
                         foreach ($topics as $topic) { 
+                            echo $topic . "<br>";
                             foreach ($db->query('SELECT id FROM topics WHERE name = "' .$topic. '" ') as $topicId) { 
                                 $db->exec('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES (' . $topicId['id'] . ', ' .$scripId['id']. ' )');
                             }
