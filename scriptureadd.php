b3ff05773f54013024afb72337dc2f80e57a0499
davault <davetheriault@gmail.com>
2015-10-21 00:50:42 -0600

    Merge origin/master
    Conflicts:
    	scriptures2.php

--- scriptureadd.php
+++ scriptureadd.php
@@ -1,4 +1,5 @@
 <?php require 'includes/dbconnection.php'; ?>
+
 <?php 
                     if (isset($_POST['book'])) {
                         try {
@@ -7,27 +8,24 @@
                         $chapter = (int)$_POST['chapter'];
                         $content = $_POST['content'];
                         $topics = $_POST['topic'];
-                        print_r($topics);
+                        
                         $db->exec('INSERT INTO scriptures (book, chapter, verse, content) VALUES ("' . $book . '", ' . $chapter.', '.$verse.', "'.$content.'") ') ;
                         
                         $scripId = $db->query('SELECT id FROM scriptures WHERE book = "' . $book . '" AND chapter = '.$chapter.' AND verse = '.$verse.' ');
                         $scripId->setFetchMode(PDO::FETCH_ASSOC);
-                        $scripId = $scripId->fetch();
+                            $scripId = $scripId->fetch();
                             
-                        echo $scripId['id'] . "<br>";
+                         
                         
                         foreach ($topics as $topic) { 
-                            echo $topic . "<br>";
                             foreach ($db->query('SELECT id FROM topics WHERE name = "' .$topic. '" ') as $topicId) { 
-                                print_r($topicId['id']);
-                                echo "<br>";
-                                $db->exec('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES (' . (int)$topicId['id'] . ', ' .(int)$scripId['id']. ')');
+                                $db->exec('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES (' . $topicId['id'] . ', ' .$scripId['id']. ' )');
                             }
                                     
                         }
                         echo "<br><br>Scripture Inserted";
                         } catch (PDOException $e)    {
-                                     echo $e->getMessage();
+                                     echo $sql . "<br>" . $e->getMessage();
                         }
                     }
                 ?>
