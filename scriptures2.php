d0e47ae88248467ca311cc22cfc616f3dd5509e7
davault <davetheriault@gmail.com>
2015-10-21 02:37:22 -0600

    Insert Statement Breakdown

--- scriptures2.php
+++ scriptures2.php
@@ -13,7 +13,7 @@ require 'includes/dbconnection.php';
             
             
             <div>
-                <form id="addScripture" action="scriptureadd.php" method="post">
+                <form id="addScripture" action="scriptures2.php" method="post">
                     Book: <input type="text" name="book"><br>
                     Chapter: <input type="number" name="chapter"><br>
                     Verse Number: <input type="number" name="verse"><br>
@@ -30,7 +30,43 @@ require 'includes/dbconnection.php';
                 
                 </form>
             </div>
-            <div>
+           
+            <?php 
+                    if (isset($_POST['book'])) {
+                        try {
+                        $verse = (int)$_POST['verse'];
+                        $book = $_POST['book'];
+                        $chapter = (int)$_POST['chapter'];
+                        $content = $_POST['content'];
+                        $topics = $_POST['topic'];
+                        
+                        $db->exec('INSERT INTO scriptures (book, chapter, verse, content) VALUES ("' . $book . '", ' . $chapter.', '.$verse.', "'.$content.'") ') ;
+
+                        $scripId = $db->query('SELECT id FROM scriptures WHERE book = "' . $book . '" AND chapter = '.$chapter.' AND verse = '.$verse.' ');
+                        $scripId->setFetchMode(PDO::FETCH_ASSOC);
+                        $scripId = $scripId->fetch();
+                        $sId = (int)$scripId['id'];
+                        
+                        foreach ($topics as $topic) { 
+                            foreach ($db->query('SELECT id FROM topics WHERE name = "' .$topic. '" ') as $topicId) {
+                                
+                                $tId = (int)$topicId['id'];
+                                echo $tId;
+                                echo '<br>' . $sId . '<br><br>';
+                                $db->exec('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES ('.$tId.', '.$sId.')');
+                            }    
+                        }
+                        
+                        include 'scripture.php';
+                        
+                        echo "<br><br>Scripture Inserted";
+                        } catch (PDOException $e)    {
+                                     echo $sql . "<br>" . $e->getMessage();
+                        }
+                    }
+                ?>
+
+
                 
                 
      
