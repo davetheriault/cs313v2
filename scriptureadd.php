03f4eeeab11817e85148670ae9e60d990b25a806
davault <davetheriault@gmail.com>
2015-10-21 08:17:58 -0600

    Insert Statement Breakdown

--- scriptureadd.php
+++ scriptureadd.php
@@ -1,6 +1,6 @@
 <h1>Scripture Resources</h1>
             <?php 
-            foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row){
+            foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures ORDER BY book ASC') as $row){
                 echo '<div class="block"><strong>'. $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
                         . $row['content'] . '"</div><br><br>';
             }
