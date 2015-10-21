ff3324871085d9a2faf70c5dd198996479cce5ed
davault <davetheriault@gmail.com>
2015-10-21 01:52:58 -0600

    Insert Statement Breakdown

--- scriptureadd.php
+++ scriptureadd.php
@@ -14,23 +14,15 @@
                         $scripId = $db->query('SELECT id FROM scriptures WHERE book = "' . $book . '" AND chapter = '.$chapter.' AND verse = '.$verse.' ');
                         $scripId->setFetchMode(PDO::FETCH_ASSOC);
                         $scripId = $scripId->fetch();
-                        
+                        $sId = $scripId['id'];
                         echo $scripId['id'];
                         
                         foreach ($topics as $topic) { 
                             foreach ($db->query('SELECT id FROM topics WHERE name = "' .$topic. '" ') as $topicId) {
                                 
-                                echo '<br>' . $topicId['id'];
-                                
-                                $stmt  = 'INSERT INTO topic_verse_link ';
-                                $stmt .= '(topic_id, scripture_id) ';
-                                $stmt .= 'VALUES (';
-                                $stmt .= $topicId['id'];
-                                $stmt .= ', ';
-                                $stmt .= $scripId['id'];
-                                $stmt .= ')';
-                                        
-                                $db->exec($stmt);
+                                $tId = $topicId['id'];
+                                      
+                                $db->exec('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES ('.$tId.', '.$tId.')');
                             }    
                         }
                         
