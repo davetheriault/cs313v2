da0cb31e49dfcc3149ba4360ad95fb4523aea2b8
davetheriault <davetheriault@gmail.com>
2015-10-20 22:07:11 -0600

    Update scriptures2.php

--- scriptures2.php
+++ scriptures2.php
@@ -3,7 +3,7 @@ require 'includes/dbconnection.php';
 ?>
 
 
-<?php $title = 'CS313 Team Readiness Scriptures'; ?>
+<?php $title = 'CS313 Team Readiness Scriptures 2'; ?>
             
             <?php include 'includes/header.php'; ?>
             
@@ -13,22 +13,45 @@ require 'includes/dbconnection.php';
             
             
             <div>
-                <form id="addScripture" action="scriptures2.php">
-                    Book: <input type="text" name="book">
-                    Chapter: <input type="number" name="chapter">
-                    Verse Number: <input type="number" name="verse">
-                    Content: <textarea form="addScripture" cols="100" rows="7"></textarea>
+                <form id="addScripture" action="scriptures2.php" method="post">
+                    Book: <input type="text" name="book"><br>
+                    Chapter: <input type="number" name="chapter"><br>
+                    Verse Number: <input type="number" name="verse"><br>
+                    Content: <textarea form="addScripture" cols="100" rows="7"></textarea><br>
                     Topics: <br>
-                    <?php 
+                    <?php
                     foreach ($db->query('SELECT name FROM topics') as $row){
-                        echo '<input type="checkbox" name="topic" value="' . $row['name'] . '">' . $row['name'] . '<br>';
-                    } ?>
+                        echo '<input type="checkbox" name="topic[]" value="' . $row['name'] . '">' . $row['name'] . '<br>';
+            }
+                    ?>
                     
+                    <input type="submit" value="Submit">
              
                 
                 </form>
             </div>
-
+            <div>
+                <?php 
+                    if (isset($_POST['book'])) {
+                        $verse = $_POST['verse'];
+                        $book = $_POST['book'];
+                        $chapter = $_POST['chapter'];
+                        $content = $_POST['content'];
+                        $topics = $_POST['topic'];
+                        $db->query('INSERT INTO scriptures (book, chapter, verse, content) VALUES ("'.$book.'", "'.$chapter.'", "'.$verse.'", "'.$content.'"') ;
+                        
+                        $scripId = $db->query('SELECT id FROM scriptures WHERE book = "' . $book . '" AND chapter = "'.$chapter.'" AND verse = "'.$verse.'" ');
+                        foreach ($topics as $topic) { 
+                            $topicID[] = $db->query('SELECT id FROM topics WHERE name = "' .$topic. '" '); 
+                        }
+                        foreach ($topicID as $topId) { 
+                            $db->query('INSERT INTO topic_verse_link (topic_id, scripture_id) VALUES ("' . $topID . '", "' .$scripID. '" )');
+                        }    
+                    }
+                ?>
+     
+        </div>
+        </div>
         
     </body>
 </html>
