479e49d218de7b37cac3eec8cea260f9c0f12aa2
davault <davetheriault@gmail.com>
2015-10-21 09:31:48 -0600

    Insert Statement Breakdown

--- scriptureadd.php
+++ scriptureadd.php
@@ -2,14 +2,14 @@
             <?php 
             foreach ($db->query('SELECT id, book, chapter, verse, content FROM scriptures ORDER BY book ASC') as $row){
                 echo '<div class="block"><strong>'. $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</strong> - "'
-                        . $row['content'] . '" <br> Related Topics:<br>';
+                        . $row['content'] . '" <br> <div style="font-size: 0.6em;">Related Topics:<br>';
                 foreach ($db->query('SELECT topic_id FROM topiclink WHERE scripture_id = "' . $row['id'] . '"') as $top){
                     foreach ($db->query('SELECT name FROM topics WHERE id = "'.$top['topic_id'].'"') as $topname) {
                         echo $topname['name'] . "<br>";
                     }
                 }
                 
-                echo '</div><br><br>';
+                echo '</div></div><br><br>';
             }
             
             ?>
