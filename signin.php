6e8c63492cdd949d39816d1e9780990ee19790fd
davault <davetheriault@gmail.com>
2015-10-27 22:33:55 -0600

    week 7 team

--- signin.php
+++ signin.php
@@ -2,31 +2,12 @@
     session_start();
     include 'hashthing.php';
 ?>
-<html>
-    <head>
-        <title>Sign In</title>
-    
-    </head>
-    <body>
-        <h1>Log In</h1>
-        <form name="login" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
-            Username: <input type="text" name="username" required/><br />
-            Password: <input type="password" name="password" required/><br />
-            
-            
-            <input name="submit" type="submit" value="Log in" />
-        </form>
-        <p>Don't have an account? Sign up <a href="signup.php">here!</a></p>
-        <?php
+<?php
             if (isset($_POST['submit']) && isset($_POST['username']) && isset($_POST['password'])) {
                 require 'includes/dbconnection.php';
                 
                 $results = $db->query("SELECT * FROM users WHERE username = '" . $_POST['username'] . "' LIMIT 1");
-                var_dump($results);
-                echo $results->rowCount() . "<br>";
-                $results->setFetchMode(PDO::FETCH_ASSOC);
-                    $user = $results->fetch();
-                    var_dump($user);
+       
                     
                 if ($results !== false) {
                     $results->setFetchMode(PDO::FETCH_ASSOC);
@@ -48,5 +29,21 @@
                 }
             }
         ?>
+<html>
+    <head>
+        <title>Sign In</title>
+    
+    </head>
+    <body>
+        <h1>Log In</h1>
+        <form name="login" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+            Username: <input type="text" name="username" required/><br />
+            Password: <input type="password" name="password" required/><br />
+            
+            
+            <input name="submit" type="submit" value="Log in" />
+        </form>
+        <p>Don't have an account? Sign up <a href="signup.php">here!</a></p>
+        
     </body>
 </html>
