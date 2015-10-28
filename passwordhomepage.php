665a3dea5c081b2af82056e6b74046cee7150649
davault <davetheriault@gmail.com>
2015-10-27 22:48:39 -0600

    week 7 team

--- passwordhomepage.php
+++ passwordhomepage.php
@@ -14,18 +14,13 @@
         <?php
             require('includes/dbconnection.php');
             
-            $results = $db->query("SELECT * FROM users WHERE user_id = " . $_SESSION['user']);
+            $results = $db->query("SELECT * FROM users WHERE id = " . $_SESSION['user']);
             
-            if ($results !== false && $results->rowCount() === 1) {
-                $results->setFetchMode(PDO::FETCH_ASSOC);
-                $user = $results->fetch();
+            $results->setFetchMode(PDO::FETCH_ASSOC);
+            $user = $results->fetch();
                 
-                echo $user['user_name'];
-            } else {
-                header('Location: signin.php');
-                exit();
-            }
-        ?>!
-        </h1>
+            echo $user['username'];
+             
+        ?>!</h1>
     </body>
 </html>
