a1a40e96b23599d38e97e160979c0e18d0b436c1
davault <davetheriault@gmail.com>
2015-11-01 23:23:17 -0700

    week 7 team

--- bookadd.php
+++ bookadd.php
@@ -1,13 +1,14 @@
 <?php require 'includes/library_db_connection.php'; ?>
 
 <?php 
-
+    if (isset($_POST)){
     $db->exec('INSERT INTO books (title, author, genre) '
             . 'VALUES ('
             . '"'.$_POST['title'].'", '
             . '"'.$_POST['author'].'", '
             . '"'.$_POST['genre'].'" '
             . ')');
+    }
 ?>
 
 <?php $title = 'Confirm'; ?>
@@ -18,9 +19,13 @@
                 <div id="mainBox">
                     <h1>Confirmation</h1>
                     <p>
-                        <?php echo $_POST['title']; ?> was added to inventory. <br><br>
-                       
-                    
+                        <?php 
+                        if (isset($_POST)){
+                            echo $_POST['title'] . 'was added to inventory. <br><br>';
+                        } else {
+                            echo '';
+                        }
+                        ?>
                         <a href="librarymanagement.php">Return to Management</a>
                     </p>
                 </div>
