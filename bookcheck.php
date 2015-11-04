c8e93f362ace0c0f856d1ce1315b524837e7490b
davault <davetheriault@gmail.com>
2015-11-03 22:18:16 -0700

    week 7 team

--- bookcheck.php
+++ bookcheck.php
@@ -1,5 +1,5 @@
 <?php require 'includes/library_db_connection.php'; ?>
-<?php $title = 'Edit Book'; ?>
+<?php $title = 'Check Book'; ?>
             <?php 
                         if (isset($_POST['title']) && isset($_POST['genre'])){
                             $db->exec('UPDATE books SET title="'.$_POST['title'].'", author="'.$_POST['author'].'",'
@@ -10,7 +10,7 @@
             
             <div id="mainContain">
                 <div id="mainBox">
-                    <h1>Edit Book</h1>
+                    <h1>Book Check In/Out</h1>
                     
                     <?php 
                         $id = $_POST['book'];
@@ -20,21 +20,27 @@
                         
                     ?>
                     
-                    <form id="addBook" action="bookedit.php" method="post">
+                    <form id="addBook" action="bookcheck.php" method="post">
                         <input type="hidden" name="book" value="<?php echo $id; ?>">
                         Book Title: <br>
-                        <input type="text" name="title" value="<?php echo $info['title']; ?>" class="textInput" required="required"><br><br>
-                        Author: <br>
-                        <input type="text" name="author" value="<?php echo $info['author']; ?>" class="textInput" required="required"><br><br>
-                        Genre:<br>
-                        <input type="text" name="genre" value="<?php echo $info['genre']; ?>" class="textInput" required="required"><br><br>
+                        <?php echo $info['title']; ?><br><br>
                         Availability:
-                        <?php if ($result['status'] == '1') { echo 'Available'; }
-                                        else { echo 'Unavailable<br><br>'
-                                            . 'Due Back: '.$info['date_due'].''; } ?><br>
-                        <?php echo '<form action="bookcheck.php" method="post">'
-                        . '<button name="book" type="submit" value="'.$info['id'].'">Check Out/In</button>'; ?><br>
-                        <input type="submit" value="Submit"><br>
+                        <?php 
+                        if ($result['status'] == '1') { 
+                            echo 'Available<br>'
+                               . '<form action="bookcheck.php" method="post">'
+                               . 'Check Out To:<select name="user">';
+                            foreach ($db->query('SELECT * FROM users ORDER BY last_name ASC, first_name ASC') as $option){
+                                echo '<option value="'.$option['id'].'">'.$option['last_name'].', '.$option['first_name'].'</option>';
+                            }
+                            echo '</select><br><input type="submit" value="Check Out"></form>';
+                        } else { 
+                            echo 'Unavailable<br><br>'
+                               . 'Due Back: '.$info['date_due'].'<br>'
+                               . '<form action="bookcheck.php" method="post">'
+                               . '<button name="checkin" type="submit" value="'.$info['id'].'">Check In</button>'
+                               . '</form>'; } ?><br>
+                        
                     </form>
                     
                     <h4><a href="librarymanagement.php">Return to Management</a></h4>
