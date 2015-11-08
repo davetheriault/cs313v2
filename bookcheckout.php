78d46d822535ec5f9b61e1d9e405be45193e8b23
davault <davetheriault@gmail.com>
2015-11-07 18:37:07 -0700

    finishing this thing

--- bookcheckout.php
+++ bookcheckout.php
@@ -3,7 +3,7 @@
 <?php 
 
     $date = date("Y-m-d");
-    $due = date_add(date("Y-m-d"), date_interval_create_from_date_string("14 days"));
+    $due = date("Y-m-d", strtotime("+14 days"));
     
     if (isset($_POST['user']) && isset($_POST['book'])) {
         $db->exec('UPDATE books SET user_id="'.$_POST['user'].'", status="0", date_out="'.$date.'", date_due="'.$due.'"'
