2283bef3c231979ce6058aae0055bff6b19779bc
davault <davetheriault@gmail.com>
2015-11-07 22:10:06 -0700

    finishing this thing

--- bookcheckin.php
+++ bookcheckin.php
@@ -1,7 +1,9 @@
 <?php require 'includes/library_db_connection.php'; ?>
 <?php
+    if (isset($_POST['book'])){
+       $db->exec('UPDATE books SET user_id="1", status="1", date_due="0000-00-00", date_out="0000-00-00" WHERE id = "'.$_POST['book'].'"');
 
-    $db->exec('UPDATE books SET user_id="1", status="1", date_due="0000-00-00", date_out="0000-00-00" WHERE id = "'.$_POST['book'].'"');
+    }
 
 ?>
 <?php header('Location: librarymanagement.php');
