32b844422a8addf028f270ee3aca7ee6807cdd33
davault <davetheriault@gmail.com>
2015-11-07 19:44:10 -0700

    finishing this thing

--- bookcheckout.php
+++ bookcheckout.php
@@ -3,7 +3,7 @@
 <?php 
 
     $date = date("Y-m-d");
-    $due = date("Y-m-d", strtotime("+1 week"));
+    $due = date("Y-m-d", strtotime("+2 weeks"));
     
     if (isset($_POST['user']) && isset($_POST['book'])) {
         $db->exec('UPDATE books SET user_id="'.$_POST['user'].'", status="0", date_out="'.$date.'", date_due="'.$due.'"'
